603b8014a9020159cb9fdc95155c05fe43ec401a
jonprobert <jonprobert@gmail.com>
2026-03-27 21:04:55 +0000

    Quartz sync: Mar 27, 2026, 9:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Raleway",
+        body: "Crimson Text",
         code: "IBM Plex Mono",
       },
       colors: {
